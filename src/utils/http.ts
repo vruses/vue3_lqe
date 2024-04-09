cf9879df400d31c71a3a94e8567754b86c89a513
vurses <1634302894@qq.com>
2024-04-09 23:19:47 +0800

    chore: 项目配置与初始化

---
+++ src/utils/http.ts
@@ -0,0 +1,139 @@
+import axios, { type AxiosRequestConfig } from 'axios'
+// import useUserStore from '@/store/modules/user'
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_API
+axios.defaults.timeout = 20 * 1000
+axios.defaults.maxBodyLength = 5 * 1024 * 1024
+axios.defaults.withCredentials = true
+
+// 添加请求拦截器
+axios.interceptors.request.use(
+  (config: AxiosRequestConfig | any) => {
+    // 避免浏览器对请求进行缓存
+    config.params = {
+      ...config.params,
+      t: Date.now(),
+    }
+    // //每次请求携带登录返回的token
+    // if (useUserStore().token) {
+    //   config.headers.token = useUserStore().token
+    // }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+// 添加响应拦截器
+axios.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+export interface ResultType<T> {
+  code: number
+  data?: T
+  msg: string
+  err?: string
+}
+
+interface Http {
+  get<T>(url: string, params?: unknown): Promise<T>
+
+  post<T>(url: string, params?: unknown): Promise<T>
+
+  upload<T>(url: string, params: unknown): Promise<T>
+
+  put<T>(url: string, params: unknown): Promise<T>
+
+  delete<T>(url: string, params: unknown): Promise<T>
+
+  download(url: string): void
+}
+
+const http: Http = {
+  get(url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url, { params })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  post(url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url, JSON.stringify(params))
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  put(url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(url, JSON.stringify(params))
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  delete(url, params) {
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(url, { params })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  upload(url, file) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url, file, {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        })
+        .then((res) => {
+          resolve(res.data)
+        })
+        .catch((err) => {
+          reject(err.data)
+        })
+    })
+  },
+
+  download(url) {
+    const iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    iframe.src = url
+    iframe.onload = function () {
+      document.body.removeChild(iframe)
+    }
+
+    document.body.appendChild(iframe)
+  },
+}
+
+export default http
